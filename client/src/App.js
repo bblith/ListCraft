899ab98ba541b8632a67fbca9bce8180058c73c5
JonathonDavis <112988057+JonathonDavis@users.noreply.github.com>
2024-07-22 04:41:15 -0500

    Backend talks to frontend
    Backend works (enough)
    Needs to be prettified for front end use and more code is needed to make the feature match our plan

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,26 @@ import TitleHeader from './TitleHeader';
 import Navbar from './Navbar';
 import Dashboard from './Dashboard';
 import Footer from './Footer';
+import ShowTodoList from './components/showTodoList';
+import { BrowserRouter, Route, Routes } from "react-router-dom"; 
+import CreateTodo from './components/createTodo';
 
 function App() {
   return (
     <div className="App">
       <TitleHeader />
       <Navbar />
-      <Dashboard />
+      <Dashboard /> 
+      <BrowserRouter>
+                <Routes>   
+                    <Route exact path="/" element={<ShowTodoList/>} />  
+                    <Route path="/create-todo" element={<CreateTodo/>} /> 
+                </Routes>
+            </BrowserRouter>
       <Footer />
     </div>
   );
 }
 
 export default App;
+
